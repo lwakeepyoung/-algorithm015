819a0fd9ad1af6a0aacbe6e9efe017fce5733f28
liweiao <liweiao@doumob.com>
2020-08-30 20:09:02 +0800

    作业

---
+++ Week_01/code/ListNode.java
@@ -0,0 +1,18 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        this.val = x;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
